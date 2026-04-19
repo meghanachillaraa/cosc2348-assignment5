ab37f1418cc1bfc0dba61bcb50a03cda5b67216f
Meghana Chillara <meghana.chillara@outlook.com>
2026-04-18 23:52:28 -0500

    Add Makefile, tests, and github.txt for Assignment 6

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -3,7 +3,10 @@ from calculator_adapter import run
 
 
 ### ADD AT LEAST TWO TESTS HERE!
-
+# Checks taht the program outputs "7" for an input of "10 - 3"
+assert run("10 - 3").output == "7"
+# Checks that the program outputs "5" for an input of "20 / 4"
+assert run("20 / 4").output == "5"
 
 ###
 
